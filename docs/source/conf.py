08f33e516624d4a53f1680130d36819a2d81c019
Arkananta Masarief <arkanantam27@gmail.com>
2026-02-05 12:03:50 +0700

    fix: version error fix on docs and pyproject

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'ReconGraph'
 copyright = '2026, Muhammad Nur Yasir Utomo'
 author = 'Muhammad Nur Yasir Utomo'
-release = '0.0.1'
+release = '0.0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
